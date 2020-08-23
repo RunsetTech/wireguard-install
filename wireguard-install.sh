e0d645a78eea69d382e0e1b56241bc90d7faee66
RunsetTech <52034765+RunsetTech@users.noreply.github.com>
2020-08-23 13:33:50 +0430

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -92,7 +92,7 @@ if [ ! -f "$WG_CONFIG" ]; then
 	apt-get install software-properties-common -y
 	add-apt-repository ppa:wireguard/wireguard -y
 	apt update
-	apt install linux-headers-$(uname -r) wireguard qrencode iptables-persistent -y
+	DEBIAN_FRONTEND=noninteractive apt-get -yq install linux-headers-$(uname -r) wireguard qrencode iptables-persistent -y
     elif [ "$DISTRO" == "Debian" ]; then
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
